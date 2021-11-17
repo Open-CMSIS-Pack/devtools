b85caaa4a8608f2cb737efcc984fcc1150c4f9f7
Sourabh Mehta <73165318+soumeh01@users.noreply.github.com>
2021-11-17 10:37:27 +0100

    list all cmake targets (#50)

---
+++ cmake/GetCMakeTargets.cmake
@@ -0,0 +1,30 @@
+function(get_all_targets var)
+    set(targets)
+    get_all_targets_recursive(targets ${CMAKE_CURRENT_SOURCE_DIR})
+    set(${var} ${targets} PARENT_SCOPE)
+endfunction()
+
+macro(get_all_targets_recursive targets dir)
+    get_property(subdirectories DIRECTORY ${dir} PROPERTY SUBDIRECTORIES)
+    foreach(subdir ${subdirectories})
+        get_all_targets_recursive(${targets} ${subdir})
+    endforeach()
+
+    get_property(current_targets DIRECTORY ${dir} PROPERTY BUILDSYSTEM_TARGETS)
+    list(APPEND ${targets} ${current_targets})
+endmacro()
+
+function(get_targets)
+  # prepare list of targets
+  get_all_targets(all_targets)
+
+  # sort target name alphabetically
+  list(SORT all_targets)
+  list(TRANSFORM all_targets PREPEND "  -- ")
+  list(TRANSFORM all_targets APPEND "\n")
+  list(INSERT all_targets 0 "CMake Targets ${CMAKE_BINARY_DIR}\n")
+  string (REPLACE ";" "" all_targets "${all_targets}")
+  
+  # write to file
+  file(WRITE ${CMAKE_BINARY_DIR}/targets "${all_targets}")
+endfunction()
