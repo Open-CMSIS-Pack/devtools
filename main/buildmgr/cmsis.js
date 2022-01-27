0235dff81fe4fbb8f49831125113698779ac2bf4
Jonatan Antoni <jonatan.antoni@arm.com>
2022-01-27 12:14:29 +0100

    Initial contribution of generated documentation

---
+++ main/buildmgr/cmsis.js
@@ -0,0 +1,7 @@
+function writeHeader() {
+    document.write('Version 0.10.3.54');
+};
+
+function writeFooter()  {
+    document.write('Generated on 2022-01-26T15:36:18Z for CMSIS-Build 0.10.3+p54-g6bf27ed by Arm Ltd. All rights reserved.');
+};
