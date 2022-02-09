8cc51d17e204661d7c8a4d9bfb19812169ccb6a1
Thorsten de Buhr <thorstendb-ARM@users.noreply.github.com>
2022-02-09 15:59:12 +0100

    packchk : multiple include pdsc seems not possible
    Issue caused by a wrong early exit.
    
    Tested:
    M051: Adding PDSC File: 'D:/Packs/Keil/STM32F1xx_DFP/2.3.0/Keil.STM32F1xx_DFP.pdsc'
    M064: Checking for other PDSC files: 'D:/Packs/Keil/STM32F1xx_DFP/2.3.0/Keil.STM32F1xx_DFP.pdsc' OK OK
    M051: Adding PDSC File: 'D:/Packs/ARM/CMSIS/5.8.0/ARM.CMSIS.pdsc' OK
    M051: Adding PDSC File: 'D:/Packs/Keil/STM32F2xx_DFP/2.9.0/Keil.STM32F2xx_DFP.pdsc' OK
    M051: Adding PDSC File: 'D:/Packs/Keil/STM32F4xx_DFP/2.15.0/Keil.STM32F4xx_DFP.pdsc' OK
    
    Fixes issue #112.

--- tools/packchk/test/integtests/src/PackChkIntegTests.cpp
+++ tools/packchk/test/integtests/src/PackChkIntegTests.cpp
@@ -195,3 +195,80 @@ TEST_F(PackChkIntegTests, SuppressValidationMsgs) {
     }
   }
 }
+
+TEST_F(PackChkIntegTests, AddRefPacks) {
+  const char* argv[10];
+
+  string outDir = PackChkIntegTestEnv::testoutput_dir + "/Packs";
+  ASSERT_TRUE(RteFsUtils::CreateDirectories(outDir));
+
+  string refNameTest = "TestPack";
+  string refName1 = "RefPack1";
+  string refName2 = "RefPack2";
+  string refName3 = "RefPack3";
+  string refName4 = "RefPack4";
+
+  string refPackTest = "/" + refNameTest;
+  string refPack1 = "/" + refName1;
+  string refPack2 = "/" + refName2;
+  string refPack3 = "/" + refName3;
+  string refPack4 = "/" + refName4;
+  ASSERT_TRUE(RteFsUtils::CreateDirectories(outDir + refPackTest));
+  ASSERT_TRUE(RteFsUtils::CreateDirectories(outDir + refPack1));
+  ASSERT_TRUE(RteFsUtils::CreateDirectories(outDir + refPack2));
+  ASSERT_TRUE(RteFsUtils::CreateDirectories(outDir + refPack3));
+  ASSERT_TRUE(RteFsUtils::CreateDirectories(outDir + refPack4));
+
+  refPackTest += "/" + refNameTest + ".pdsc";
+  refPack1 += "/" + refName1 + ".pdsc";
+  refPack2 += "/" + refName2 + ".pdsc";
+  refPack3 += "/" + refName3 + ".pdsc";
+  refPack4 += "/" + refName4 + ".pdsc";
+  const string contentBegin = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"\
+                              "<package schemaVersion=\"1.3\" xmlns:xs=\"http://www.w3.org/2001/XMLSchema-instance\" xs:noNamespaceSchemaLocation=\"PACK.xsd\">\n"\
+                              "  <name>";    
+  const string contentEnd =   "  </name>\n"\
+                              "</package>\n";
+
+  refPackTest = outDir + refPackTest;
+  refPack1 = outDir + refPack1;
+  refPack2 = outDir + refPack2;
+  refPack3 = outDir + refPack3;
+  refPack4 = outDir + refPack4;
+
+  ASSERT_TRUE(RteFsUtils::CreateFile(refPackTest, contentBegin + refNameTest + contentEnd));
+  ASSERT_TRUE(RteFsUtils::CreateFile(refPack1, contentBegin + refName1 + contentEnd));
+  ASSERT_TRUE(RteFsUtils::CreateFile(refPack2, contentBegin + refName2 + contentEnd));
+  ASSERT_TRUE(RteFsUtils::CreateFile(refPack3, contentBegin + refName3 + contentEnd));
+  ASSERT_TRUE(RteFsUtils::CreateFile(refPack4, contentBegin + refName4 + contentEnd));
+
+  ASSERT_TRUE(RteFsUtils::Exists(refPackTest));
+  ASSERT_TRUE(RteFsUtils::Exists(refPack1));
+  ASSERT_TRUE(RteFsUtils::Exists(refPack2));
+  ASSERT_TRUE(RteFsUtils::Exists(refPack3));
+  ASSERT_TRUE(RteFsUtils::Exists(refPack4));
+
+  argv[0] = (char*)"";
+  argv[1] = (char*)refPackTest.c_str();
+  argv[2] = (char*)"-i";
+  argv[3] = (char*)refPack1.c_str();
+  argv[4] = (char*)"-i";
+  argv[5] = (char*)refPack2.c_str();
+  argv[6] = (char*)"-i";
+  argv[7] = (char*)refPack3.c_str();
+  argv[8] = (char*)"-i";
+  argv[9] = (char*)refPack4.c_str();
+
+  PackChk packChk;
+  packChk.Check(10, argv, nullptr);
+  
+  const RteGlobalModel& model = packChk.GetModel();
+  const RtePackageMap& packs = model.GetPackages();
+
+  for(auto& [name, pack] : packs) {
+    if(name != refNameTest && name != refName1 && name != refName2 && name != refName3 && name != refName4) {
+      FAIL() << "RefPack was not added";
+    }
+  }
+
+}
