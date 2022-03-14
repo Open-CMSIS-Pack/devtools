43d8b82101c8720ba2f7b27500c4ee91d71a62f9
grasci <86058054+grasci-arm@users.noreply.github.com>
2022-03-14 13:01:59 +0100

    [buildmgr] Update cpackget utility v0.3.1

--- tools/buildmgr/cbuildgen/installer/create_installer.sh
+++ tools/buildmgr/cbuildgen/installer/create_installer.sh
@@ -69,7 +69,7 @@ cp -R ../config/. ${distdir}/etc
 cp ../../docs/LICENSE.txt ${distdir}
 
 # Get cpackget
-cpackget_version="0.3.0"
+cpackget_version="0.3.1"
 cpackget_base=https://github.com/Open-CMSIS-Pack/cpackget/releases/download/v${cpackget_version}/cpackget_${cpackget_version}
 curl --retry 3 -L ${cpackget_base}_windows_amd64.zip   -o temp.zip && unzip -p temp.zip '*/cpackget.exe' > ${distdir}/bin/cpackget.exe && rm temp.zip
 curl --retry 3 -L ${cpackget_base}_linux_amd64.tar.gz  -o - | tar xzfO - --wildcards    '*cpackget'     > ${distdir}/bin/cpackget.lin
