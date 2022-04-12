ccd0d70fb4a8cc1c7724408cf4b351692b335f78
Reinhard Keil <reinhard.keil@arm.com>
2022-04-12 13:41:31 +0200

    Project Template: initial version added

---
+++ tools/projmgr/docs/Template/MyProject/main.c
@@ -0,0 +1,19 @@
+/*----------------------------------------------------------------------------
+ * CMSIS bare-metal 'main' function template
+ *---------------------------------------------------------------------------*/
+ 
+#include "RTE_Components.h"          // component selection 
+#include  CMSIS_device_header        // device header file
+ 
+ 
+int main (void) {
+ 
+  // System Initialization
+  SystemCoreClockUpdate();
+  // ... other initalizations
+
+  // Application code starts here
+  for (;;) {
+    // ...
+	}
+}
