e90ce103f21c6c96aa36588c7baf65d61eeafaab
Tarek BOCHKATI <tarek.bouchkati@st.com>
2022-05-11 16:42:03 +0200

    [projmgr] ProjMgrGenerator: add GenerateCprjOptions
    Contributed by STMicroelectronics
    Signed-off-by: Tarek BOCHKATI <tarek.bouchkati@st.com>

--- tools/projmgr/include/ProjMgrGenerator.h
+++ tools/projmgr/include/ProjMgrGenerator.h
@@ -41,6 +41,7 @@ protected:
   void GenerateCprjTarget(XMLTreeElement* element, const ContextItem& context);
   void GenerateCprjComponents(XMLTreeElement* element, const ContextItem& context);
   void GenerateCprjGroups(XMLTreeElement* element, const std::vector<GroupNode>& groups, const std::string& compiler);
+  void GenerateCprjOptions(XMLTreeElement* element, const BuildType& buildType);
   void GenerateCprjMisc(XMLTreeElement* element, const std::vector<MiscItem>& misc, const std::string& compiler);
   void GenerateCprjLinkerScript(XMLTreeElement* element, const std::string& compiler, const std::string& linkerScript);
   void GenerateCprjVector(XMLTreeElement* element, const std::vector<std::string>& vec, std::string tag);
