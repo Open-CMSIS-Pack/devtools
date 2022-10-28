8de20378cae03d99cd921330a7e416d7c2f90859
grasci <86058054+grasci-arm@users.noreply.github.com>
2022-10-28 08:14:55 +0100

    CMSIS_PACK_ROOT directory for windows

--- libs/crossplatform/src/Windows/constants.h
+++ libs/crossplatform/src/Windows/constants.h
@@ -10,7 +10,7 @@
 constexpr const char* LOCAL_APP_DATA = "LOCALAPPDATA";
 constexpr const char* USER_PROFILE   = "USERPROFILE";
 constexpr const char* PACK_ROOT_DIR  = "\\Arm\\Packs";
-constexpr const char* CACHE_DIR      = "";
+constexpr const char* CACHE_DIR      = "\\AppData\\Local";
 constexpr const char* HOST_TYPE      = "win";
 
 #endif // CROSSPLATFORM_CONSTANTS_H
