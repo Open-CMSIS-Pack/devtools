207d1d46aaeeeaee2d8549934969eb52c23fbd7e
Tarek BOCHKATI <tarek.bouchkati@st.com>
2022-10-31 16:10:50 +0100

    use -Oz for both AC6 and GCC
    Signed-off-by: Tarek BOCHKATI <tarek.bouchkati@st.com>

--- tools/buildmgr/cbuildgen/config/AC6.6.18.0.cmake
+++ tools/buildmgr/cbuildgen/config/AC6.6.18.0.cmake
@@ -47,7 +47,7 @@ function(cbuild_set_defines lang defines)
 endfunction()
 
 set(OPTIMIZE_VALUES       "none" "balanced" "size" "speed")
-set(OPTIMIZE_CC_FLAGS     "-O0"  "-O2"      "-Os"  "-O3")
+set(OPTIMIZE_CC_FLAGS     "-O0"  "-O2"      "-Oz"  "-O3")
 set(OPTIMIZE_CXX_FLAGS    ${OPTIMIZE_CC_FLAGS})
 set(OPTIMIZE_AS_ARM_FLAGS ${OPTIMIZE_CC_FLAGS})
 set(OPTIMIZE_AS_GNU_FLAGS ${OPTIMIZE_CC_FLAGS})
