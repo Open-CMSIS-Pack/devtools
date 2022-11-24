b7268370d8356ee6d6272ebb046b9805023c5612
grasci <86058054+grasci-arm@users.noreply.github.com>
2022-11-24 15:23:08 +0000

    [buildmgr] Handle legacy armasm hexadecimal defines

--- tools/buildmgr/test/testinput/Examples/AC6/Asm/AsmArm.s
+++ tools/buildmgr/test/testinput/Examples/AC6/Asm/AsmArm.s
@@ -5,4 +5,12 @@
      INFO   1, "_RTE_ is not defined!"
      ENDIF
 
+     IF     HEXADECIMAL_TEST != 11259375 ; 0xABCDEF
+     INFO   1, "HEXADECIMAL_TEST failed!"
+     ENDIF
+
+     IF     DECIMAL_TEST != 1234567890
+     INFO   1, "DECIMAL_TEST failed!"
+     ENDIF
+     
      END
