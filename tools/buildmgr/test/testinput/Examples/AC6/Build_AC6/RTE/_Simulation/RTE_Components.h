3cdd7add3b5c96262bbd87836d1c031653b0edcb
Daniel Brondani <daniel.brondani@arm.com>
2023-01-23 10:56:36 +0100

    [buildmgr] Add AC6 test examples RTE files

---
+++ tools/buildmgr/test/testinput/Examples/AC6/Build_AC6/RTE/_Simulation/RTE_Components.h
@@ -0,0 +1,27 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Simulation' 
+ * Target:  'Simulation' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "ARMCM3.h"
+
+/* ARM::CMSIS:RTOS2:Keil RTX5:Library:5.5.4 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+/* Keil.ARM Compiler::Compiler:I/O:STDOUT:ITM:1.2.0 */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+          #define RTE_Compiler_IO_STDOUT_ITM      /* Compiler I/O: STDOUT ITM */
+
+
+#endif /* RTE_COMPONENTS_H */
