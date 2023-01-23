3cd2f56205b0187abe8029972a0039f0b1e34473
grasci <86058054+grasci-arm@users.noreply.github.com>
2023-01-23 14:08:10 +0000

    [buildmgr] Add AC6 test examples RTE files

---
+++ tools/buildmgr/test/testinput/Examples/AC6/TrustZone/CM33_ns/RTE/_FVP_Simulation_Model/RTE_Components.h
@@ -0,0 +1,25 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'FVP_Simulation_Model' 
+ * Target:  'FVP_Simulation_Model' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "ARMCM33_DSP_FP_TZ.h"
+
+/* ARM::CMSIS:RTOS2:Keil RTX5:Library_NS:5.5.4 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+        #define RTE_CMSIS_RTOS2_RTX5_ARMV8M_NS  /* CMSIS-RTOS2 Keil RTX5 Armv8-M Non-secure domain */
+
+
+#endif /* RTE_COMPONENTS_H */
