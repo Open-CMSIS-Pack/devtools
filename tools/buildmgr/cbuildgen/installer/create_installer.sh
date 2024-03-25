7692c06db3939738ec40612992563cb79ef0bb4c
Daniel Brondani <daniel.brondani@arm.com>
2024-03-25 13:00:57 +0100

    [buildmgr] Add `cbuild2cmake` utility, update versions

--- tools/buildmgr/cbuildgen/installer/create_installer.sh
+++ tools/buildmgr/cbuildgen/installer/create_installer.sh
@@ -61,7 +61,7 @@ cp -R ../config/. ${distdir}/etc
 cp ../../docs/LICENSE.txt ${distdir}
 
 # Get cpackget
-cpackget_version="2.0.0"
+cpackget_version="2.1.0"
 cpackget_base=https://github.com/Open-CMSIS-Pack/cpackget/releases/download/v${cpackget_version}/cpackget_${cpackget_version}
 curl --retry 3 -L ${cpackget_base}_linux_amd64.tar.gz  -o - | tar xzfO - --wildcards    '*cpackget'     > ${distdir}/bin/cpackget.lin-amd64
 curl --retry 3 -L ${cpackget_base}_windows_amd64.zip   -o temp.zip && unzip -p temp.zip '*/cpackget.exe' > ${distdir}/bin/cpackget.exe-amd64 && rm temp.zip
@@ -70,8 +70,18 @@ curl --retry 3 -L ${cpackget_base}_linux_arm64.tar.gz  -o - | tar xzfO - --wildc
 curl --retry 3 -L ${cpackget_base}_windows_arm64.zip   -o temp.zip && unzip -p temp.zip '*/cpackget.exe' > ${distdir}/bin/cpackget.exe-arm64 && rm temp.zip
 curl --retry 3 -L ${cpackget_base}_darwin_arm64.tar.gz -o - | tar xzfO - --wildcards    '*cpackget'     > ${distdir}/bin/cpackget.mac-arm64
 
+# Get cbuild2cmake
+cbuild2cmake_version="0.9.0-dev0"
+cbuild2cmake_base=https://github.com/Open-CMSIS-Pack/cbuild2cmake/releases/download/v${cbuild2cmake_version}/cbuild2cmake_${cbuild2cmake_version}
+curl --retry 3 -L ${cbuild2cmake_base}_linux_amd64.tar.gz  -o - | tar xzfO - --wildcards    '*cbuild2cmake'     > ${distdir}/bin/cbuild2cmake.lin-amd64
+curl --retry 3 -L ${cbuild2cmake_base}_windows_amd64.zip   -o temp.zip && unzip -p temp.zip '*/cbuild2cmake.exe' > ${distdir}/bin/cbuild2cmake.exe-amd64 && rm temp.zip
+curl --retry 3 -L ${cbuild2cmake_base}_darwin_amd64.tar.gz -o - | tar xzfO - --wildcards    '*cbuild2cmake'     > ${distdir}/bin/cbuild2cmake.mac-amd64
+curl --retry 3 -L ${cbuild2cmake_base}_linux_arm64.tar.gz  -o - | tar xzfO - --wildcards    '*cbuild2cmake'     > ${distdir}/bin/cbuild2cmake.lin-arm64
+curl --retry 3 -L ${cbuild2cmake_base}_windows_arm64.zip   -o temp.zip && unzip -p temp.zip '*/cbuild2cmake.exe' > ${distdir}/bin/cbuild2cmake.exe-arm64 && rm temp.zip
+curl --retry 3 -L ${cbuild2cmake_base}_darwin_arm64.tar.gz -o - | tar xzfO - --wildcards    '*cbuild2cmake'     > ${distdir}/bin/cbuild2cmake.mac-arm64
+
 # Get cbridge
-cbridge_version="0.9.2"
+cbridge_version="0.9.3"
 cbridge_base=https://github.com/Open-CMSIS-Pack/generator-bridge/releases/download/v${cbridge_version}/cbridge_${cbridge_version}
 curl --retry 3 -L ${cbridge_base}_linux_amd64.tar.gz  -o - | tar xzfO - --wildcards    '*cbridge'     > ${distdir}/bin/cbridge.lin-amd64
 curl --retry 3 -L ${cbridge_base}_windows_amd64.zip   -o temp.zip && unzip -p temp.zip '*/cbridge.exe' > ${distdir}/bin/cbridge.exe-amd64 && rm temp.zip
@@ -81,7 +91,7 @@ curl --retry 3 -L ${cbridge_base}_windows_arm64.zip   -o temp.zip && unzip -p te
 curl --retry 3 -L ${cbridge_base}_darwin_arm64.tar.gz -o - | tar xzfO - --wildcards    '*cbridge'     > ${distdir}/bin/cbridge.mac-arm64
 
 # Get csolution
-csolution_version="2.2.1"
+csolution_version="2.3.0-dev0"
 csolution_base=https://github.com/Open-CMSIS-Pack/devtools/releases/download/tools%2Fprojmgr%2F${csolution_version}/projmgr.zip
 curl --retry 3 -L ${csolution_base} -o temp.zip && unzip -q -d temp temp.zip
 cp 'temp/bin/linux-amd64/csolution' ${distdir}/bin/csolution.lin-amd64
@@ -94,7 +104,7 @@ cp -r temp/etc/* ${distdir}/etc
 rm temp.zip && rm -rf temp
 
 # Get cbuild
-cbuild_version="2.2.1"
+cbuild_version="2.3.0-dev0"
 cbuild_base=https://github.com/Open-CMSIS-Pack/cbuild/releases/download/v${cbuild_version}/cbuild_${cbuild_version}
 curl --retry 3 -L ${cbuild_base}_linux_amd64.tar.gz  -o - | tar xzfO - --wildcards    '*cbuild'     > ${distdir}/bin/cbuild.lin-amd64
 curl --retry 3 -L ${cbuild_base}_windows_amd64.zip   -o temp.zip && unzip -p temp.zip '*/cbuild.exe' > ${distdir}/bin/cbuild.exe-amd64 && rm temp.zip
