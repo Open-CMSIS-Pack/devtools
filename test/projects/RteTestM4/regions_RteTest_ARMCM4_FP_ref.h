c395f884aa7a747d4a5a175f2bd2bbb464678ffb
Joachim Krech <8290187+jkrech@users.noreply.github.com>
2024-08-05 18:07:08 +0200

    Update reference regions file

--- test/projects/RteTestM4/regions_RteTest_ARMCM4_FP_ref.h
+++ test/projects/RteTestM4/regions_RteTest_ARMCM4_FP_ref.h
@@ -4,12 +4,13 @@
 
 //-------- <<< Use Configuration Wizard in Context Menu >>> --------------------
 
-// <n>Device pack:   ARM::RteTest_DFP@0.2.0
-// <i>Device pack used to generate this file
+// Auto-generated file: USE CONFIGURATION WIZARD TO CHANGE VALUES!
+// <n> Generated using information from packs
+// <i> Device Family Pack (DFP):   ARM::RteTest_DFP@0.2.0
 
-// <h>ROM Configuration
+// <h> ROM Configuration
 // =======================
-// <h> FLASH=<__ROM0>
+// <h> __ROM0 (is region: FLASH from DFP)
 //   <o> Base address <0x0-0xFFFFFFFF:8>
 //   <i> Defines base address of memory region.
 //   <i> Default: 0x00000000
@@ -18,19 +19,19 @@
 //   <i> Defines size of memory region.
 //   <i> Default: 0x00040000
 #define __ROM0_SIZE 0x00040000
-//   <q>Default region
+//   <q> Default region
 //   <i> Enables memory region globally for the application.
 #define __ROM0_DEFAULT 1
-//   <q>Startup
+//   <q> Startup
 //   <i> Selects region to be used for startup code.
 #define __ROM0_STARTUP 1
 // </h>
 
 // </h>
 
-// <h>RAM Configuration
+// <h> RAM Configuration
 // =======================
-// <h> SRAM=<__RAM0>
+// <h> __RAM0 (is region: SRAM from DFP)
 //   <o> Base address <0x0-0xFFFFFFFF:8>
 //   <i> Defines base address of memory region.
 //   <i> Default: 0x20000000
@@ -39,22 +40,23 @@
 //   <i> Defines size of memory region.
 //   <i> Default: 0x00020000
 #define __RAM0_SIZE 0x00020000
-//   <q>Default region
+//   <q> Default region
 //   <i> Enables memory region globally for the application.
 #define __RAM0_DEFAULT 1
-//   <q>No zero initialize
+//   <q> No zero initialize
 //   <i> Excludes region from zero initialization.
 #define __RAM0_NOINIT 1
 // </h>
 
 // </h>
 
-// <h>Stack / Heap Configuration
+// <h> Stack / Heap Configuration
 //   <o0> Stack Size (in Bytes) <0x0-0xFFFFFFFF:8>
 //   <o1> Heap Size (in Bytes) <0x0-0xFFFFFFFF:8>
 #define __STACK_SIZE 0x00000200
-#define __HEAP_SIZE 0x00000C00
+#define __HEAP_SIZE 0x00000000
 // </h>
 
 
 #endif /* REGIONS_RTETEST_ARMCM4_FP_H */
+
