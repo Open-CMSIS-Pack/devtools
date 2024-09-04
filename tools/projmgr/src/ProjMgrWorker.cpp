c1c54507f6925f7bb902c4ca3b578c145fe6d3bc
Sourabh Mehta <73165318+soumeh01@users.noreply.github.com>
2024-09-04 13:00:48 +0200

    Updated error message

--- tools/projmgr/src/ProjMgrWorker.cpp
+++ tools/projmgr/src/ProjMgrWorker.cpp
@@ -1715,7 +1715,7 @@ bool ProjMgrWorker::ProcessToolchain(ContextItem& context) {
 
   // get compatible supported toolchain
   if (!GetToolchainConfig(context.toolchain.name, context.toolchain.range, context.toolchain.config, context.toolchain.version)) {
-    m_toolchainErrors.insert("cmake configuration file missing in CMSIS_COMPILER_ROOT for compiler '" + context.compiler + "'");
+    m_toolchainErrors.insert("no toolchain cmake files found. Check ./etc directory and verify CMSIS-Toolbox installation");
     context.toolchain.version = RteUtils::GetPrefix(context.toolchain.range);
   }
 
@@ -4387,7 +4387,7 @@ bool ProjMgrWorker::ListToolchains(vector<ToolchainItem>& toolchains) {
       // list registered toolchains
       GetRegisteredToolchains();
       if (m_toolchains.empty()) {
-        ProjMgrLogger::Error("compiler registration environment variable missing, format: <GCC|CLANG|AC6|IAR>_TOOLCHAIN_<major>_<minor>_<patch>, or CMSIS_COMPILER_ROOT may need to be set");
+        ProjMgrLogger::Error("no compiler registered. Add path to compiler ./bin directory with environment variable <name>_TOOLCHAIN_<major>_<minor>_<patch>. <name> is one of AC6, GCC, IAR, CLANG");
         return false;
       }
       toolchains = m_toolchains;
