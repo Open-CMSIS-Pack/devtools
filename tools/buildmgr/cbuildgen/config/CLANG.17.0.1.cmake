72ee0947468ab127716198d866a4d56e2f1e0b0a
Daniel Brondani <daniel.brondani@arm.com>
2024-09-13 17:25:28 +0200

    [devtools] Extend `language-C` accepted values: `c17` and `c23` (#1032)

--- tools/buildmgr/cbuildgen/config/CLANG.17.0.1.cmake
+++ tools/buildmgr/cbuildgen/config/CLANG.17.0.1.cmake
@@ -57,9 +57,9 @@ set(WARNINGS_ASM_FLAGS    ""   "-w"  "-Wall")
 set(WARNINGS_CXX_FLAGS    ""   "-w"  "-Wall")
 set(WARNINGS_LD_FLAGS     ""   "-w"  "-Wall")
 
-set(LANGUAGE_VALUES       "c90"      "gnu90"      "c99"      "gnu99"      "c11"      "gnu11"      "c++98"      "gnu++98"      "c++03"      "gnu++03"      "c++11"      "gnu++11"      "c++14"      "gnu++14"      "c++17"      "gnu++17"      "c++20"      "gnu++20"     )
-set(LANGUAGE_CC_FLAGS     "-std=c90" "-std=gnu90" "-std=c99" "-std=gnu99" "-std=c11" "-std=gnu11" ""           ""             ""           ""             ""           ""             ""           ""             ""           ""             ""           ""            )
-set(LANGUAGE_CXX_FLAGS    ""         ""           ""         ""           ""         ""           "-std=c++98" "-std=gnu++98" "-std=c++03" "-std=gnu++03" "-std=c++11" "-std=gnu++11" "-std=c++14" "-std=gnu++14" "-std=c++17" "-std=gnu++17" "-std=c++20" "-std=gnu++20")
+set(LANGUAGE_VALUES       "c90"      "gnu90"      "c99"      "gnu99"      "c11"      "gnu11"      "c17"      "c23"      "c++98"      "gnu++98"      "c++03"      "gnu++03"      "c++11"      "gnu++11"      "c++14"      "gnu++14"      "c++17"      "gnu++17"      "c++20"      "gnu++20"     )
+set(LANGUAGE_CC_FLAGS     "-std=c90" "-std=gnu90" "-std=c99" "-std=gnu99" "-std=c11" "-std=gnu11" "-std=c17" "-std=c23" ""           ""             ""           ""             ""           ""             ""           ""             ""           ""             ""           ""            )
+set(LANGUAGE_CXX_FLAGS    ""         ""           ""         ""           ""         ""           ""         ""         "-std=c++98" "-std=gnu++98" "-std=c++03" "-std=gnu++03" "-std=c++11" "-std=gnu++11" "-std=c++14" "-std=gnu++14" "-std=c++17" "-std=gnu++17" "-std=c++20" "-std=gnu++20")
 
 function(cbuild_set_option_flags lang option value flags)
   if(NOT DEFINED ${option}_${lang}_FLAGS)
