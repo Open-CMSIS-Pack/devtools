5410b29d0e849389aeaf30ab0f986f9d4d82237a
Sourabh Mehta <73165318+soumeh01@users.noreply.github.com>
2025-02-10 13:49:43 +0100

    Update tools/projmgr/src/ProjMgr.cpp

--- tools/projmgr/src/ProjMgr.cpp
+++ tools/projmgr/src/ProjMgr.cpp
@@ -48,7 +48,7 @@ Options:\n\
   -m, --missing                 List only required packs that are missing in the pack repository\n\
   -n, --no-check-schema         Skip schema check\n\
   -N, --no-update-rte           Skip creation of RTE directory and files\n\
-  -o,-O --output arg            Base folder for output files, 'outdir' and 'tmpdir' (default \"./\")\n\
+  -o,-O --output arg            Base folder for output files, 'outdir' and 'tmpdir' (default: \"Same as '*.csolution.yml'\")\n\
   -q, --quiet                   Run silently, printing only error messages\n\
   -R, --relative-paths          Print paths relative to project or ${CMSIS_PACK_ROOT}\n\
   -S, --context-set             Select the context names from cbuild-set.yml for generating the target application\n\
