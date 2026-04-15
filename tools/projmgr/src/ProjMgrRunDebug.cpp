c3e12cc3cdb0d0bd5119a007e71d8a3c6adfbc2f
Jen-Tse Huang <jen-tse.huang@arm.com>
2026-04-15 15:40:54 +0200

    Fix for unit tests

--- tools/projmgr/src/ProjMgrRunDebug.cpp
+++ tools/projmgr/src/ProjMgrRunDebug.cpp
@@ -32,6 +32,7 @@ ProjMgrRunDebug::~ProjMgrRunDebug(void) {
 }
 
 bool ProjMgrRunDebug::CollectSettings(const vector<ContextItem*>& contexts, const DebugAdaptersItem& adapters) {
+  m_runDebug = RunDebugType{};  // reset all fields to avoid carry-over between runs
 
   // get target settings
   const auto& context0 = contexts.front();
