c2e5f7d6e426deb1970c2f581e27ba0b28231344
Jen-Tse Huang <jen-tse.huang@arm.com>
2026-04-15 17:31:49 +0200

    Add the `board-pack-path:` and `device-pack-path:` in cbuild-run.yml

--- tools/projmgr/src/ProjMgrCbuildRun.cpp
+++ tools/projmgr/src/ProjMgrCbuildRun.cpp
@@ -47,8 +47,14 @@ ProjMgrCbuildRun::ProjMgrCbuildRun(YAML::Node node,
   SetNodeValue(node[YAML_COMPILER], debugRun.compiler);
   SetNodeValue(node[YAML_BOARD], debugRun.board);
   SetNodeValue(node[YAML_BOARD_PACK], debugRun.boardPack);
+  if (!debugRun.boardPackPath.empty()) {
+    SetNodeValue(node[YAML_BOARD_PACK_PATH], FormatPath(debugRun.boardPackPath, m_directory));
+  }
   SetNodeValue(node[YAML_DEVICE], debugRun.device);
   SetNodeValue(node[YAML_DEVICE_PACK], debugRun.devicePack);
+  if (!debugRun.devicePackPath.empty()) {
+    SetNodeValue(node[YAML_DEVICE_PACK_PATH], FormatPath(debugRun.devicePackPath, m_directory));
+  }
   SetFilesNode(node[YAML_OUTPUT], debugRun.outputs);
   SetResourcesNode(node[YAML_SYSTEM_RESOURCES], debugRun.systemResources);
   SetFilesNode(node[YAML_SYSTEM_DESCRIPTIONS], debugRun.systemDescriptions);
