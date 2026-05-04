36a26a3bc73d6a5411f340e0cd81c65fb1c1ee08
Sourabh Mehta <sourabh.mehta@arm.com>
2026-05-04 08:46:07 +0200

    Update to safe HTTPS

--- cmake/GetGitRevisionDescription.cmake
+++ cmake/GetGitRevisionDescription.cmake
@@ -33,7 +33,7 @@
 #
 # Original Author:
 # 2009-2020 Ryan Pavlik <ryan.pavlik@gmail.com> <abiryan@ryand.net>
-# http://academic.cleardefinition.com
+# https://academic.cleardefinition.com
 #
 # Copyright 2009-2013, Iowa State University.
 # Copyright 2013-2020, Ryan Pavlik
@@ -41,7 +41,7 @@
 # SPDX-License-Identifier: BSL-1.0
 # Distributed under the Boost Software License, Version 1.0.
 # (See accompanying file LICENSE_1_0.txt or copy at
-# http://www.boost.org/LICENSE_1_0.txt)
+# https://www.boost.org/LICENSE_1_0.txt)
 
 if(__get_git_revision_description)
     return()
