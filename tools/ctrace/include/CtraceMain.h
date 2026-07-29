b7358b68f9adff898e39305f71e458a5962469c8
Thorsten de Buhr <thorsten.deBuhr@arm.com>
2026-07-29 11:28:12 +0200

    Add initial ctrace tool integration

---
+++ tools/ctrace/include/CtraceMain.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2026 Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef CTRACE_MAIN_H
+#define CTRACE_MAIN_H
+
+int CtraceMain();
+
+#endif // CTRACE_MAIN_H
---
+++ tools/ctrace/src/main.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2026 Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "CtraceMain.h"
+
+int main() {
+  return CtraceMain();
+}
