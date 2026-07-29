d7bf54021ca0624534de4e3831aada0018ef4e82
Thorsten de Buhr <thorsten.deBuhr@arm.com>
2026-07-29 15:37:34 +0200

    Ctrace (#2547)
    ## Changes
    - Initial workflow commit for new tool ctrace

---
+++ tools/ctrace/src/CtraceMain.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2026 Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "CtraceMain.h"
+
+#include <cstdio>
+
+int CtraceMain() {
+  std::printf("ctrace Executable\n");
+  return 0;
+}
